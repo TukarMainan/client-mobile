e7c61df6632edd04f3e7c4e73ad6a1dd8cac962c
michaelroring <roringmichael@gmail.com>
2023-05-09 23:15:00 +0700

    feat: add talkjs

--- clientmobile/components/Chat.jsx
+++ clientmobile/components/Chat.jsx
@@ -1,55 +1,86 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet , Image} from 'react-native';
-import { GiftedChat , InputToolbar} from 'react-native-gifted-chat';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { GiftedChat, InputToolbar } from "react-native-gifted-chat";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import * as TalkRn from "@talkjs/expo";
 
 const initialMessages = [
   {
     _id: 1,
-    text: 'Hello! How can I help you today?',
+    text: "Hello! How can I help you today?",
     createdAt: new Date(),
     user: {
       _id: 2,
-      name: 'John Doe',
-      avatar: 'https://placeimg.com/140/140/any',
+      name: "John Doe",
+      avatar: "https://placeimg.com/140/140/any",
     },
   },
   // Add more initial messages as needed
 ];
-import cameraIcon from '../filter.png';
 
-export default function ChatScreen({ navigation }) {
+export default function ChatScreen(obj) {
+  // console.log("obj :", obj.route.params);
+  const id = obj.route.params.id;
+  const name = obj.route.params.name;
+  const photoUrl = obj.route.params.photoUrl;
+
   const [messages, setMessages] = useState(initialMessages);
 
   function onSend(newMessages) {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages));
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
   }
 
   function navigateToMessages() {
     navigation.goBack();
   }
 
+  const me = {
+    id: "123456789",
+    name: "Alice",
+    email: "alice@example.com",
+    photoUrl: "https://talkjs.com/images/avatar-1.jpg",
+    welcomeMessage: "Hey there! How are you? :-)",
+    role: "default",
+  };
+
+  const other = {
+    id,
+    name,
+    email: "Sebastian@example.com",
+    photoUrl,
+    welcomeMessage: "Hey, how can I help? https://google.com",
+    role: "default",
+  };
+
+  const conversationBuilder = TalkRn.getConversationBuilder(
+    TalkRn.oneOnOneId(me, other)
+  );
+
+  conversationBuilder.setParticipant(me);
+  conversationBuilder.setParticipant(other);
+
   function renderInputToolbar(props) {
     return (
       <InputToolbar {...props}>
-        <TouchableOpacity onPress={() => console.log('Camera button pressed')}>
-              <Icon name="camera"  size={24} style={styles.icon}/>
-              <Icon name="file-image-outline"  size={24} style={styles.icon} />
-              <Icon name="camera"  size={24} style={styles.icon} />
+        <TouchableOpacity onPress={() => console.log("Camera button pressed")}>
+          <Icon name="camera" size={24} style={styles.icon} />
+          <Icon name="file-image-outline" size={24} style={styles.icon} />
+          <Icon name="camera" size={24} style={styles.icon} />
         </TouchableOpacity>
       </InputToolbar>
     );
   }
   return (
     <View style={styles.container}>
-      <GiftedChat
-        messages={messages}
-        onSend={messages => onSend(messages)}
-        user={{
-          _id: 1,
-        }}
-        renderInputToolbar={renderInputToolbar}
-      />
+      <TalkRn.Session appId="tPI8y4El" me={me}>
+        {/* <TalkRn.ConversationList
+          me={me}
+          onSelectConversation={() => navigateToChat()}
+        /> */}
+        <TalkRn.Chatbox conversationBuilder={conversationBuilder} />
+      </TalkRn.Session>
     </View>
   );
 }
@@ -59,15 +90,15 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   backButton: {
-    position: 'absolute',
+    position: "absolute",
     top: 40,
     left: 20,
     zIndex: 1,
   },
   backButtonText: {
     fontSize: 16,
-    fontWeight: 'bold',
-    color: 'black',
+    fontWeight: "bold",
+    color: "black",
   },
   cameraIcon: {
     width: 24,
@@ -76,6 +107,6 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginHorizontal: 10,
-    color: '#6F6F6F',
+    color: "#6F6F6F",
   },
 });
