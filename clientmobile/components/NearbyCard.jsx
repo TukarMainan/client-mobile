e76efe4bd826af682e57703afa9dd0cbf94e9b23
wahidfebr <wahidfebr@gmail.com>
2023-05-12 06:31:37 +0700

    feat: add style report and wip style

--- clientmobile/components/NearbyCard.jsx
+++ clientmobile/components/NearbyCard.jsx
@@ -45,15 +45,27 @@ export default function NearbyCard({ item }) {
   return (
     <View style={styles.container}>
       <Image source={{ uri: item?.images[0] }} style={styles.image} />
-      <Text style={styles.category}>{locationName}</Text>
+      <Text style={styles.location}>{locationName}</Text>
       <Text style={styles.name}>{item?.title}</Text>
-      <Text style={styles.category}>{item?.Category.name}</Text>
+      <Text style={styles.location}>{item?.Category.name}</Text>
       <Text style={styles.category}>{timePostCreatedInString}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  location: {
+    fontSize: 12,
+    // fontWeight: "bold",
+    backgroundColor: "#FFF8E7",
+    width: 150,
+    // textAlign: "center",
+    color: "#333333",
+    paddingTop: 8,
+    paddingBottom: 12,
+    textAlign: "left",
+    paddingLeft: 20,
+  },
   container: {
     flex: 1,
     alignItems: "center",
